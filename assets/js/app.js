b8d3fc0152b5c8fe369f021505ac7a41fb2c4713
amirasahraee <amirali.sf77@gmail.com>
2021-07-15 19:57:06 +0430

    add section  about and posts

---
+++ assets/js/app.js
@@ -0,0 +1,26 @@
+let toggle;
+
+const load = () => {
+    const menuToggle = document.querySelector('.menuToggle');
+    const menuIcon = document.getElementById('menuIcon');
+    const navigation = document.body.querySelector('.navigation');
+    // for opening menu
+    toggle = () => {
+        menuToggle.classList.toggle('active');
+        navigation.classList.toggle('active');
+
+
+        if (menuToggle.classList.contains('active')) {
+            menuIcon.classList.remove('fa-bars');
+            menuIcon.classList.add('fa-close');
+        } else {
+            menuIcon.classList.remove('fa-close');
+            menuIcon.classList.add('fa-bars');
+        }
+    }
+
+    window.addEventListener('scroll', function() {
+        const header = document.querySelector('header');
+        header.classList.toggle('sticky', window.scrollY > 0);
+    })
+}
